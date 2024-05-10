f27ae98aee47a09c4382717d1afa67c70ee1d0a6
Anozie Joel Chibuike <chibuikeanozie0@gmail.com>
2024-05-10 23:15:17 +0100

    Update gay.py

--- gay.py
+++ gay.py
@@ -15,7 +15,7 @@ root.title('Gay game')
 root.resizable(width=False,height=False)
 root['bg'] = 'white'
 
-label = Label(root, text='Are you gay?', font='Arial 20 bold', bg='white').pack()
+label = Label(root, text='Are you  gay?', font='Arial 20 bold', bg='white').pack()
 btnYes = Button(root,text='No',font='Arial 20 bold')
 btnYes.place(x=170,y=100)
 btnYes.bind('<Enter>', motionMouse)
